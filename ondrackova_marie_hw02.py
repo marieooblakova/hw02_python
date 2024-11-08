ca5a94389e0ef5ec333bd7a71acc2ba8a57f07f2
marieooblakova <marils@seznam.cz>
2024-11-08 15:50:24 +0100

    Add files via upload

--- ondrackova_marie_hw02.py
+++ ondrackova_marie_hw02.py
@@ -6,22 +6,27 @@ with open( 'netflix_titles.tsv', encoding = 'utf-8') as file:
         line = line.strip().split('\t')
         netflix_list.append(line)     
 
-netflix_list = netflix_list[1:] # odmazání záhlaví
-
 output = [] # seznam na slovníky
+netflix_list = netflix_list[1:] # odmazání záhlaví
 
 for line in netflix_list:
     netflix_dict = {}
     netflix_dict['title']  = line[2]
     directors = line[15].split(',') # vytvoření seznamu režisérů ze stringu
+    if directors ==['']:#převedení seznamu s prázdným stringem na prázdný seznam
+        directors = []
     netflix_dict['director'] = directors
-    actors = line[16].split(',')
+    actors = line[16].split(', ')
+    if actors == ['']:#převedení seznamu s prázdným stringem na prázdný seznam
+        directors = []
+        actors = []
     netflix_dict['cast'] = actors 
-    netflix_dict['decade'] = line[5][:3] + '0'
+    netflix_dict['decade'] =int(line[5][:3] + '0')
     genre_list = line[8].split(',')
     netflix_dict['genre'] = genre_list
     output.append(netflix_dict)  
 
 
-with open('hw02_output.json','w', encoding='utf-8') as newfile:
+with open('hw02_output_opr1.json','w', encoding='utf-8') as newfile:
     json.dump(output, newfile, indent=4)          
+      
